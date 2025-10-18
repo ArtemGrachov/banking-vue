61efb807060aedf9e731bbb907c778de3caad03f
Artem Grachov <grachov2195@gmail.com>
2025-10-18 14:58:05 +0300

    feat: header, footer, default layout, BDA-51

--- src/layouts/layout-default/LayoutDefault.vue
+++ src/layouts/layout-default/LayoutDefault.vue
@@ -7,12 +7,20 @@ import Header from '@/layouts/layout-default/components/Header.vue';
 <template>
   <div class="layout">
     <Header class="header" />
-    <router-view></router-view>
+    <div class="container">
+      <aside class="sidebar">
+        @todo user navigation menu
+      </aside>
+      <router-view></router-view>
+    </div>
     <Footer class="footer" />
   </div>
 </template>
 
 <style lang="scss" scoped>
+@use '/src/styles/mixins/layout.scss' as layout;
+@use '/src/styles/mixins/breakpoints.scss' as breakpoints;
+
 .layout {
   min-height: 100%;
   display: flex;
@@ -21,6 +29,28 @@ import Header from '@/layouts/layout-default/components/Header.vue';
 
 .header {
   flex: 0 0 auto;
+  position: sticky;
+  top: 0;
+  left: 0;
+}
+
+.container {
+  @include layout.container();
+
+  @include breakpoints.lg() {
+    display: flex;
+    align-items: stretch;
+  }
+}
+
+.sidebar {
+  display: none;
+
+  @include breakpoints.lg() {
+    display: block;
+    width: 320px;
+    flex: 0 0 auto;
+  }
 }
 
 .footer {
