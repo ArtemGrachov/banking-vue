61a62a3758669917b2e4ef9d105667356636a192
Artem Grachov <grachov2195@gmail.com>
2025-10-18 13:52:46 +0300

    feat: default layout, BDA-51

---
+++ src/layouts/layout-default/LayoutDefault.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import Footer from '@/layouts/layout-default/components/Footer.vue';
+import Header from '@/layouts/layout-default/components/Header.vue';
+
+</script>
+
+<template>
+  <div class="layout">
+    <Header class="header" />
+    <router-view></router-view>
+    <Footer class="footer" />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.layout {
+  min-height: 100%;
+  display: flex;
+  flex-direction: column;
+}
+
+.header {
+  flex: 0 0 auto;
+}
+
+.footer {
+  flex: 0 0 auto;
+  margin-top: auto;
+}
+</style>
