90cd4f91b2fe55e0831e17c2e8172cbe39c261b6
Artem Grachov <grachov2195@gmail.com>
2025-10-28 01:35:33 +0200

    feat: charts (wip), BDA-12

---
+++ src/views/charts/ViewCharts.vue
@@ -0,0 +1,43 @@
+<script lang="ts" setup>
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import { Pie } from 'vue-chartjs';
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+const data = {
+  labels: ['VueJs', 'EmberJs', 'ReactJs', 'AngularJs'],
+  datasets: [
+    {
+      backgroundColor: ['#41B883', '#E46651', '#00D8FF', '#DD1B16'],
+      data: [40, 20, 80, 10],
+    },
+  ],
+};
+
+const options = {
+  responsive: true,
+  maintainAspectRatio: false,
+};
+
+</script>
+
+<template>
+  <div class="page">
+    <div class="chart">
+      <Pie :data="data" :options="options" />
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@use '/src/styles/mixins/layout.scss' as layout;
+
+.page {
+  @include layout.page();
+}
+
+.chart {
+  max-width: 800px;
+  margin: 0 auto;
+}
+</style>
